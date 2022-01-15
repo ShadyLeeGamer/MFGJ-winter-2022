3edd2361bfee30a95ef1d298fa43f5c911fd554e
gemaco1397 <85459110+gemaco1397@users.noreply.github.com>
2022-01-15 17:49:39 +0100

    Implemented auto regen on plants

--- MFGJ Winter 2022/Assets/Programming/Crops/CropController.cs
+++ MFGJ Winter 2022/Assets/Programming/Crops/CropController.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class CropController : MonoBehaviour
 {
     [SerializeField] float health;
+    [SerializeField] float regenPSec;
+    [SerializeField] float AutoHealDelay;
     public bool alive = true;
     float _health;
-
+    float healthStore;
+    bool attacked;
     public delegate void killCrop();
-
+    float timer;
     public event killCrop killed;
     [SerializeField] Gradient healthcolors;
     SpriteRenderer SR;
@@ -22,10 +25,42 @@ public class CropController : MonoBehaviour
         SR = gameObject.GetComponentInChildren<SpriteRenderer>();
     }
 
+
+    public void Update()
+    {
+        if (healthStore > _health)
+        {
+            healthStore = _health;
+            timer = AutoHealDelay;
+        }
+        else
+        {
+            if (attacked)
+            {
+                timer -= Time.deltaTime;
+                if(timer <= 0)
+                {
+                    attacked = false;
+                }
+            }
+            else
+            {
+                _health += regenPSec * Time.deltaTime;
+                SR.color = healthcolors.Evaluate(_health / health);
+                if (_health > health)
+                {
+                    _health = health;
+                }
+            }
+            healthStore = _health;
+
+        }
+    }
+
     public void attackCrop(float damage)
     {
         _health -= damage * Time.deltaTime;
-        
+        attacked = true;
         SR.color = healthcolors.Evaluate(_health / health);
         if(_health < 0)
         {
